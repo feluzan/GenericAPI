3a3e9ac07f383546a12e8df39143a3c57eb413da
FELIXSIGEXNB\feluz <feluzan@gmail.com>
2024-01-04 16:51:42 -0300

    Changing to Springboot application

---
+++ src/test/java/io/github/feluzan/GenericAPI/GenericApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.feluzan.GenericAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
